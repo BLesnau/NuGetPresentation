91f51ab60f0a95e0e1dff41e30fb676035bd5611
Brett Lesnau <lesnaubr@gmail.com>
2014-10-05 01:40:09 -0400

    .NET app done

---
+++ DotNetAppDone/DotNetApp/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Windows;
+using Allan.Language.Detection;
+
+namespace DotNetApp
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void Button_Click( object sender, RoutedEventArgs e )
+      {
+         var languages = Detector.DetectLanguage(inputTextBox.Text);
+         var topLanguage = languages.OrderBy( l => l.Distance ).First();
+         var langString = new CultureInfo( topLanguage.Language.Code ).DisplayName;
+
+         languageTextBox.Content = langString;
+      }
+   }
+}
